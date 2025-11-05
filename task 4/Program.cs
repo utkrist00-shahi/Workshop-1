0911b766be3155cf1df9dfc4d42d6ff70f1eade1
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-05 12:23:36 +0545

    task 4 completed

---
+++ task 4/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] nums = { 1, 3, 4, 5, 9 };
+Array.Sort(nums);
+
+foreach (int num in nums)
+{
+    Console.WriteLine(num);
+}
+
+Array.Reverse(nums);
+foreach (int num in nums)
+{
+    Console.WriteLine(num);
+}
+
+int index = Array.IndexOf(nums, 5);
+Console.WriteLine($"\nIndex of 5: {index}");
