ada151a3922423028ff1b84dbeafe571b9147b56
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-05 12:31:29 +0545

    task 6 completed

---
+++ task 6/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> fruits = new List<string> { "Apple", "Mango", "Banana" };
+
+fruits.Add("Orange");
+
+fruits.Remove("Banana");
+
+Console.WriteLine("List of Fruits:");
+foreach (string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+Dictionary<int, string> fruitDictionary = new Dictionary<int, string>
+        {
+            { 1, "Apple" },
+            { 2, "Mango" },
+            { 3, "Orange" }
+        };
+
+fruitDictionary.Add(4, "Grapes");
+
+
+Console.WriteLine("\nFruit Dictionary:");
+foreach (KeyValuePair<int, string> entry in fruitDictionary)
+{
+    Console.WriteLine($"ID: {entry.Key}, Name: {entry.Value}");
+}
