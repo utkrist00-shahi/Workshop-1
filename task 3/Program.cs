81a67a5a3b918a3b9b841de04de975a53bc11a23
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-05 12:19:34 +0545

    task 3 completed

---
+++ task 3/Program.cs
@@ -0,0 +1,44 @@
+﻿// task 3
+byte b;
+b = 0;
+
+short s;
+s = 0;
+
+int i;
+i = 42;
+
+long l;
+l = 1;
+
+float f;
+f = 2.56f;
+
+double d;
+d = 14.55;
+
+char c;
+c = 'q';
+
+bool value;
+value = false;
+
+
+string sValue = i.ToString();
+
+string aValue = "3.14";
+
+double dValue = Convert.ToDouble(aValue);
+
+
+Console.WriteLine("byte b = " + b);
+Console.WriteLine("short s = " + s);
+Console.WriteLine("int i = " + i);
+Console.WriteLine("long l = " + l);
+Console.WriteLine("float f = " + f);
+Console.WriteLine("double d = " + d);
+Console.WriteLine("char c = " + c);
+Console.WriteLine("bool value = " + value);
+Console.WriteLine("string sValue (from int) = " + sValue);
+Console.WriteLine("string aValue = " + aValue);
+Console.WriteLine("double dValue (converted from string) = " + dValue);
